f31470b441e62bc7f799aaa41119506ce2a9d2aa
Canaan <reakak1@gmail.com>
2022-10-07 14:52:54 +0300

    traits can be generic and implemented as needed ?! nice !

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
 
     let to_do_item: Result<ItemType, &'static str> = to_do_factory("pending", "cooking");
     match to_do_item.unwrap() {
-        ItemType::Pending(item) => item.Create(&item.super_struct.title),
+        ItemType::Pending(item) => item.create(&item.super_struct.title),
         ItemType::Done(item) => println!("Its a {} item with the title {}.", item.super_struct.status, item.super_struct.title),
     }
 
