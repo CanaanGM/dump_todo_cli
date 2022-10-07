bc7deaa8dbd369821da47d147474aaa2513b29f8
Canaan <reakak1@gmail.com>
2022-10-07 14:35:53 +0300

    structs and a factory to get them off the streets and employ them!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod to_do;
+
+use to_do::ItemType;
+use to_do::to_do_factory;
+
 fn main() {
+    let to_do_item: Result<ItemType, &'static str> = to_do_factory("done", "make");
+    match to_do_item.unwrap() {
+        ItemType::Pending(item) => println!("Its a {} item with the title {}.", item.super_struct.status, item.super_struct.title),
+        ItemType::Done(item) => println!("Its a {} item with the title {}.", item.super_struct.status, item.super_struct.title),
+    }
+
     println!("Hello, world!");
 }
